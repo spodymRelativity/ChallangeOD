131678ce1ca076e13e5cfcf7326f3b349913a814
Tomasz Spodymek <tomasz.spodymek@relativity.com>
2023-04-20 13:34:26 +0200

    aaa

---
+++ Challenge/Challenge/OddNumberSelector.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Challenge
+{
+    internal class OddNumberSelector
+    {
+        /*
+            The stream contains integers in the <0, 1 000 000> range. Return the ones that occur an odd number of times.
+
+            Attention
+                The amount of data exceeds 1GB
+
+            Limitations:
+                - memory consumption < 200 MB
+                - execution time < 1 min
+        */
+
+        internal IEnumerable<int> Execute(Stream fs)
+        {
+            throw new System.NotImplementedException();
+
+            using (StreamReader sr = new StreamReader(fs))
+            {
+                string line;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+
+                }
+            }
+        }
+    }
+}
--- Challenge/Challenge/Program.cs
+++ Challenge/Challenge/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 
 namespace Challenge
 {
@@ -6,7 +9,33 @@ namespace Challenge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"c:\work\dev\tmp\test\test3.txt";
+            IEnumerable<int> result;
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            using (FileStream fs = File.OpenRead(path))
+            {
+                OddNumberSelector counter = new OddNumberSelector();
+                result = counter.Execute(fs);
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine("Elapsed Time is {0} ms", stopwatch.Elapsed.ToString());
+
+            Console.WriteLine("CHECKSUM:");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            foreach (var item in result)
+            {
+                Console.Write((char)(item % 100));
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.ReadLine();
         }
     }
 }
