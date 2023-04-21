6335522c30bd48be1b9c2b79387c51747b3fa781
Tomasz Spodymek <tomasz.spodymek@relativity.com>
2023-04-21 09:19:08 +0200

    remove solution

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -16,85 +16,9 @@ namespace ConsoleApp1
         {
 
             string path = @"c:\work\dev\tmp\test\test3.txt";
-            //Program.Generate(path);
+            Program.Generate(path);
 
-            int n = 0;
-
-            Console.WriteLine("START");
-            Stopwatch stopwatch = new Stopwatch();
-
-            stopwatch.Start();
-            //var a = File.ReadAllLines(path);
-            //var a = File.ReadLines(path);
-            //foreach(var line in a)
-            //{
-            //    ;
-            //}
-
-            var lowerBound = 1000000;
-            var upperBound = 1000000;
-            HashSet<string> set = new HashSet<string>();
-            int count = 0;
-
-            using (FileStream fs = File.OpenRead(path))
-            {
-                using (StreamReader sr = new StreamReader(fs))
-                {
-                    Dictionary<int, bool> res = new Dictionary<int, bool>();
-                    var arr = new bool[upperBound];
-                    int buffSize = 1024;
-                    char[] buffer = new char[buffSize];
-                    string line;
-                    int i = 0;
-
-                    //while((i=sr.Read())>0)
-                    //{
-
-                    //}
-
-                    while ((line = sr.ReadLine()) != null)
-                    {
-                        //var a = new String(buffer).Split("\r\n");
-                        if (!set.Remove(line))
-                        {
-                            set.Add(line);
-                        }
-                        //++n;
-                        //int nr = int.Parse(line);
-                        //arr[nr] = !arr[nr];
-                        //if (!res.ContainsKey(nr))
-                        //{
-                        //    res.Add(nr, true);
-                        //}
-                        //else
-                        //{
-                        //    res[nr] = !res[nr];
-                        //}
-                    }
-                    count = arr.Count(a => a);
-                    Console.WriteLine("CHECKSUM:" );
-                    Console.WriteLine("");
-                    Console.WriteLine("");
-
-                    for (int index=0; index < upperBound; ++index)
-                    {
-                        if(arr[index])
-                        {
-                            Console.Write((char)(index % 100));
-                        }
-                    }
-
-                    Console.WriteLine("");
-                    Console.WriteLine("");
-
-                }
-            }
             
-            stopwatch.Stop();
-
-            Console.WriteLine("Elapsed Time is {0} ms", stopwatch.Elapsed.ToString());
-
-            Console.WriteLine("STOP " + count);
             Console.ReadLine();
         }
 
